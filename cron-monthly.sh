249765b16e1fd2fc6559d8e8f294bdfb37493767
Lean Web Solutions <orders@leanws.com>
2019-02-22 21:41:30 -0500

    new life

---
+++ cron-monthly.sh
@@ -0,0 +1,26 @@
+
+npm update
+
+npm upgrade
+
+rsync -au \
+/usr/lib/node_modules/bootstrap/dist/css/bootstrap.min.css \
+/usr/lib/node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker3.min.css \
+/usr/lib/node_modules/font-awesome/css/font-awesome.min.css \
+/usr/lib/node_modules/font-awesome/fonts \
+/usr/lib/node_modules/timepicker/jquery.timepicker.min.css \
+/usr/lib/node_modules/jgrowl/jquery.jgrowl.min.css \
+/usr/lib/node_modules/jquery/dist/jquery.min.js \
+/usr/lib/node_modules/bootstrap/dist/js/bootstrap.min.js \
+/usr/lib/node_modules/bootstrap-datepicker/js/bootstrap-datepicker.js \
+/usr/lib/node_modules/timepicker/jquery.timepicker.min.js \
+/usr/lib/node_modules/jquery.cookie/jquery.cookie.js \
+/usr/lib/node_modules/js-md5/build/md5.min.js \
+/usr/lib/node_modules/moment/min/moment.min.js \
+/usr/lib/node_modules/timepicker/jquery.timepicker.min.js \
+/usr/lib/node_modules/jquery.cookie/jquery.cookie.js \
+/usr/lib/node_modules/validator/validator.min.js \
+/usr/lib/node_modules/jgrowl/jquery.jgrowl.min.js \
+/usr/lib/node_modules/knockout/build/output/knockout-latest.js \
+/mnt/junk/github/js/
+
